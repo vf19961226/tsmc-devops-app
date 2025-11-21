76a96a6dada11d3aca691eaa18cde2c32da1f6a0
皮蛋瘦肉粥 <77768660+vf19961226@users.noreply.github.com>
2025-11-21 23:43:40 +0800

    change package
    change package Prometheus.Client.AspNetCore to prometheus-net.AspNetCore

--- TsmcDevOpsApp/Program.cs
+++ TsmcDevOpsApp/Program.cs
@@ -1,22 +1,14 @@
-// Program.cs
-using Prometheus.Client.AspNetCore; // <-- 1. 新增命名空間
+using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// 2. 註冊 Prometheus Client 服務
-builder.Services.AddMetrics(); 
-
 var app = builder.Build();
 
-// 3. 註冊 Prometheus Middleware (在 UseRouting 之前)
-app.UsePrometheusMetrics(); 
+// 啟用 HTTP Request Metrics
+app.UseHttpMetrics();
 
-// 4. 暴露 Metrics 端點，通常是 /metrics
-app.UseMetricServer("/metrics"); 
+// 暴露 /metrics
+app.MapMetrics("/metrics");
 
-// 原有的應用程式端點
 app.MapGet("/", () => "Hello, TSMC DevOps Candidate! (from C# App)");
-
-// ... 其他 MapGet/MapPost
 
 app.Run();
